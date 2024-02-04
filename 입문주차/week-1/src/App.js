e693082fa4e9290bb7b606dd553fef00dc5235af
whitewise95 <81284265+whitewise95@users.noreply.github.com>
2024-02-04 22:10:25 +0900

    1-5

---
+++ 입문주차/week-1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+// import 'App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <p>Hello React !</p>
+    </div>
+  );
+}
+
+export default App;
