d471272ed348e9005edd0034027f8bb4a30da95f
whitewise95 <81284265+whitewise95@users.noreply.github.com>
2024-02-27 22:06:03 +0900

    2-8 React_memo

--- 숙련주차/week-1/src/App.jsx
+++ 숙련주차/week-1/src/App.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import GrandFather from './components/GrandFather'
+import React, { useState } from "react";
+import Box1 from "./components/Box1";
+import Box2 from "./components/Box2";
+import Box3 from "./components/Box3";
+
+const boxesStyle = {
+  display: "flex",
+  marginTop: "10px",
+};
 
 function App() {
+  console.log("App 컴포넌트가 렌더링되었습니다!");
+
+  const [count, setCount] = useState(0);
+
+  // 1을 증가시키는 함수
+  const onPlusButtonClickHandler = () => {
+    setCount(count + 1);
+  };
+
+  // 1을 감소시키는 함수
+  const onMinusButtonClickHandler = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <GrandFather></GrandFather>
-  )
+    <>
+      <h3>카운트 예제입니다!</h3>
+      <p>현재 카운트 : {count}</p>
+      <button onClick={onPlusButtonClickHandler}>+</button>
+      <button onClick={onMinusButtonClickHandler}>-</button>
+      <div style={boxesStyle}>
+        <Box1 />
+        <Box2 />
+        <Box3 />
+      </div>
+    </>
+  );
 }
 
 export default App
