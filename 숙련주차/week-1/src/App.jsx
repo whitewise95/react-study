6444e529728530bd8e1025a60bfdad536a76bdbf
whitewise95 <81284265+whitewise95@users.noreply.github.com>
2024-02-28 21:38:37 +0900

    1_9_useMemo

--- 숙련주차/week-1/src/App.jsx
+++ 숙련주차/week-1/src/App.jsx
@@ -1,46 +1,19 @@
-import React, { useCallback, useState } from "react";
-import Box1 from "./components/Box1";
-import Box2 from "./components/Box2";
-import Box3 from "./components/Box3";
-
-const boxesStyle = {
-  display: "flex",
-  marginTop: "10px",
-};
+import React from 'react'
+import HeavyComponent from './components/HeavyComponent'
 
 function App() {
-  console.log("App 컴포넌트가 렌더링되었습니다!");
-
-  const [count, setCount] = useState(0);
-
-  // 1을 증가시키는 함수
-  const onPlusButtonClickHandler = () => {
-    setCount(count + 1);
-  };
-
-  // 1을 감소시키는 함수
-  const onMinusButtonClickHandler = () => {
-    setCount(count - 1);
-  };
-
-	// count를 초기화해주는 함수
-  const initCount = useCallback(() => {
-    setCount(0);
-  }, [count]);
   
   return (
-    <>
-      <h3>카운트 예제입니다!</h3>
-      <p>현재 카운트 : {count}</p>
-      <button onClick={onPlusButtonClickHandler}>+</button>
-      <button onClick={onMinusButtonClickHandler}>-</button>
-      <div style={boxesStyle}>
-        <Box1 initCount={initCount} />
-        <Box2 />
-        <Box3 />
-      </div>
-    </>
-  );
+    <div>
+        <nav style={{
+          backgroundColor : 'yellow',
+          marginBottom : '30px'
+        }}>네비케이션 바</nav>
+        <HeavyComponent></HeavyComponent>
+        <footer>푸터 영역이에요</footer>
+
+    </div>
+  )
 }
 
 export default App
