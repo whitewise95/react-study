83c6d27e7ed2f6f93b58d3b8c4b987813e314b47
whitewise95 <81284265+whitewise95@users.noreply.github.com>
2024-03-08 22:23:56 +0900

    React Router Dom (2)

--- 숙련주차/router-app/src/pages/Works.jsx
+++ 숙련주차/router-app/src/pages/Works.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { data }  from "../shared/data";
+import { Link } from 'react-router-dom';
 
 function Works() {
     return (
         <>
-            <div>Works</div>
-            <Link to="/contect">contect 페이지로 이동</Link>
+            <div>할일 목록</div>
+            {data.map((item) => {
+               return (
+               <div key={item.id}>
+                {item.id}
+                &nbsp; 
+                <Link to={`/work/${item.id}`}>{item.todo}</Link>
+               </div>
+               );
+            })}
         </>
 
     )
